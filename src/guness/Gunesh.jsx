f50459050bfba3634fb540ac34cf611ae30b83d3
gunes <gunes@gmail.com>
2026-04-18 12:57:46 +0400

    finish

--- src/guness/Gunesh.jsx
+++ src/guness/Gunesh.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Gunesh({ad,yas}) {
   return (
     <div style={{border:'solid 1px gray', padding:"10px",margin:"10px"}}>Gunesh
-    <h1>Men {ad}, {yas}-im yasim var</h1></div>
+    <h1>Men {ad} {yas} yasim var </h1></div>
   )
 }
 
