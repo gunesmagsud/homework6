461666ec9f644359409530e9b5ef7826f9c78c0b
gunes <gunes@gmail.com>
2026-04-18 12:50:46 +0400

    salam

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Gunesh from './guness/Gunesh'
+
+function App() {
+ 
+  return (
+    <>
+    <div>
+      <h1>Salam</h1>
+      <Gunesh ad={"Gunes"} yas={13}   >
+
+        
+      </Gunesh>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, cupiditate dolores. Culpa perferendis veritatis, dolor recusandae ullam porro quaerat inventore mollitia ratione repudiandae, quis aliquam qui laudantium. Consectetur corporis sed, cumque optio itaque commodi sequi repellat veritatis vitae a sit molestias. Quam nobis nisi tempore consequuntur expedita cupiditate! Minima, odit. Ratione voluptatibus minima voluptatem rerum totam eligendi vel aliquam tempora, cumque excepturi. Veniam molestias iste dolores inventore repellat, consequatur, at voluptates accusamus doloribus voluptatibus distinctio. Iure est labore alias consequuntur sunt libero nihil saepe rem voluptate quam amet, ipsum culpa ipsa repudiandae quidem possimus! Nisi doloremque aperiam quaerat possimus aspernatur vero accusamus repellendus temporibus? Ratione itaque ducimus quae vitae excepturi! Delectus nulla rem quam quos esse! Architecto modi aperiam atque voluptates ducimus vitae cum, eum deserunt tenetur sunt incidunt et ab, at hic, natus fugit itaque. Quos quod soluta similique eos temporibus nam esse maxime, consectetur non velit sequi deserunt, sit officiis magnam sunt illum adipisci enim asperiores fugit nihil iste neque odit! Architecto, quibusdam cumque sit quos eius facilis ut voluptas nemo tempore omnis voluptatum esse molestiae, placeat dolorem quo impedit voluptatibus recusandae non quas odit veritatis sed quasi, mollitia officiis! Corporis, veniam perspiciatis quaerat nemo earum dolorem labore, perferendis amet dolores possimus quos totam aliquam blanditiis error ducimus? Rerum qui perspiciatis nobis facilis quas! Dolores quibusdam aut blanditiis porro tempore fugit voluptate iste, laudantium ex inventore recusandae placeat! Necessitatibus laboriosam ullam voluptatibus labore beatae mollitia, soluta aliquam vitae, eveniet nemo alias omnis consequatur assumenda non minus, commodi fugiat.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/guness/Gunesh.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Gunesh({ad,yas}) {
+  return (
+    <div style={{border:'solid 1px gray', padding:"10px",margin:"10px"}}>Gunesh
+    <h1>Men {ad}, {yas}-im yasim var</h1></div>
+  )
+}
+
+export default Gunesh
